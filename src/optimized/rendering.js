ce3be6160c4e2b70bbecd70d23eda5a076b871f0
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-29 21:02:21 +0200

    used back foreach loop

--- src/optimized/rendering.js
+++ src/optimized/rendering.js
@@ -23,21 +23,21 @@ function createElement(product, prodId, deleteProductFn) {
 
 export function renderProducts(products, deleteProductFn) {
   productListEl.innerHTML = '';
-  const startTime = performance.now();
-  // products.forEach(product => {
-  //   const newListEl = createElement(product, product.id, deleteProductFn);
-  //   productListEl.appendChild(newListEl);
-  // });
-  for (let i = 0; i < products.length; i++) {
-    const newListEl = createElement(
-      products[i],
-      products[i].id,
-      deleteProductFn
-    );
+  products.forEach(product => {
+    const newListEl = createElement(product, product.id, deleteProductFn);
     productListEl.appendChild(newListEl);
-  }
-  const endTime = performance.now();
-  console.log(endTime - startTime);
+  });
+  // const startTime = performance.now();
+  // for (let i = 0; i < products.length; i++) {
+  //   const newListEl = createElement(
+  //     products[i],
+  //     products[i].id,
+  //     deleteProductFn
+  //   );
+  //   productListEl.appendChild(newListEl);
+  // }
+  // const endTime = performance.now();
+  // console.log(endTime - startTime);
 }
 
 export function updateProducts(product, prodId, deleteProductFn, isAdding) {
